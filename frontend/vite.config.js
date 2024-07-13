ffdaae37621b8deeb8967dac65e42df40bc074bc
dylanmydude <dylan@euca.co.za>
2024-07-14 00:33:09 +0200

    Update .gitignore and remove cached files

---
+++ frontend/vite.config.js
@@ -0,0 +1,33 @@
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  define: {
+    'process.env': process.env,
+  },
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@tailwindConfig': path.resolve(__dirname, 'tailwind.config.js'),
+    },
+  },
+  optimizeDeps: {
+    include: ['@tailwindConfig'],
+  },
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      input: './public/index.html',
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]',
+      },
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+});
